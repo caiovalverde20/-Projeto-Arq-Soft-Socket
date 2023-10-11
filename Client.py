9e05b19104a8186e8c0089c1510bde3aa334e242
luisaadanttas <maria.luisa.dantas@ccc.ufcg.edu.br>
2023-10-11 17:10:22 -0300

    adding pre processing

--- Client.py
+++ Client.py
@@ -1,16 +1,37 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 import socket
+import numpy as np
 
-def python_processor():
+def receive_data():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 9999))  # ip e porta, talvez tenha que mudar. Tem que ser igual ao do socket
-    try:
-        while True:
-            text = client_socket.recv(1024) # numero maximo de bytes a serem recebidos de uma vez só
-            if not text:
-                break
-            print(text)
-    finally:
-        client_socket.close()
+    client_socket.connect(('localhost', 9999))  
+
+    received_data = b""  
+    while True:
+        data = client_socket.recv(1024)  
+        if not data:
+            break
+        received_data += data
+
+    text = received_data.decode('utf-8')
+
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform([text])
+    
+    vocab = tfidf_vectorizer.get_feature_names_out()
+    tfidf_values = tfidf_matrix.toarray()[0]
+
+    term_tfidf = dict(zip(vocab, tfidf_values))
+
+    sorted_terms = sorted(term_tfidf.items(), key=lambda x: x[1], reverse=True)
+
+    top_10_terms = sorted_terms[:10]
+
+    print("Os 10 termos mais frequentes:")
+    for term, tfidf in top_10_terms:
+        print(f"{term}: {tfidf}")
+
+    client_socket.close()
 
 if __name__ == "__main__":
-    python_processor()
+    receive_data()
